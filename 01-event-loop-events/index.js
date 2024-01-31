32fd37d7ac8a573b2f82e5f4c915c3ec00babe7f
Gregory Pereverzev <boot762@gmail.com>
2024-01-31 19:50:48 +0300

    refactor: add info function

--- 01-event-loop-events/index.js
+++ 01-event-loop-events/index.js
@@ -1,22 +1,49 @@
 const fs = require('fs');
+const dns = require('dns');
 
-function timeStamp() {
-  return performance.now().toFixed(2);
+function info(text) {
+  console.log(text, performance.now().toFixed(2));
 }
 
 console.log('Programm start');
 
-setTimeout(() => console.log('Timeout 1', timeStamp()), 0);
-setTimeout(() => console.log('Timeout 2', timeStamp()), 10);
+//Close events
+fs.writeFile('./test.txt', 'Hello world!', () => info('File written'));
 
-fs.writeFile('./test.txt', 'Hello world!', () =>
-  console.log('File writen', timeStamp())
-);
+//Promises
+Promise.resolve().then(() => info('Promise 1'));
 
-Promise.resolve().then(() => console.log('Promise 1', timeStamp()));
+//Next tick
+process.nextTick(() => info('Next tick 1'));
 
-process.nextTick(() => console.log('Next tick 1', timeStamp()));
+//setImmediate (Check)
+setImmediate(() => info('Immediate 1'));
 
-setImmediate(() => console.log('Immediate 1', timeStamp()));
+//Timeouts
+setTimeout(() => info('Timeout 1'), 0);
+setTimeout(() => {
+  process.nextTick(() => info('Next tick 2'));
+  info('Timeout 2');
+}, 100);
+
+//Intervals
+let intervalCount = 0;
+const myInterval = setInterval(() => {
+  info(`Interval ${(intervalCount += 1)}`);
+  if (intervalCount > 1) {
+    clearInterval(myInterval);
+  }
+}, 50);
+
+if (intervalCount > 1) {
+  clearInterval(myInterval);
+}
+
+//I/O events
+dns.lookup('localhost', (err, address, family) => {
+  info('DNS 1 localhost');
+  Promise.resolve().then(() => info('Promise 2'));
+  process.nextTick(() => info('Next tick 3'));
+});
 
 console.log('Programm end');
