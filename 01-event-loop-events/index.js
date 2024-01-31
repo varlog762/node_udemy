3250573bd601116414dcbb9d34efc167323f6942
Gregory Pereverzev <boot762@gmail.com>
2024-01-31 17:19:55 +0300

    feat: add second lesson task

--- 01-event-loop-events/index.js
+++ 01-event-loop-events/index.js
@@ -1,8 +1,22 @@
+const fs = require('fs');
+
+function timeStamp() {
+  return performance.now().toFixed(2);
+}
+
 console.log('Programm start');
 
-setTimeout(() => console.log('Timeout 1'), 0);
-setTimeout(() => console.log('Timeout 2'), 10);
+setTimeout(() => console.log('Timeout 1', timeStamp()), 0);
+setTimeout(() => console.log('Timeout 2', timeStamp()), 10);
 
-Promise.resolve().then(() => console.log('Promise 1'));
+fs.writeFile('./test.txt', 'Hello world!', () =>
+  console.log('File writen', timeStamp())
+);
+
+Promise.resolve().then(() => console.log('Promise 1', timeStamp()));
+
+process.nextTick(() => console.log('Next tick 1', timeStamp()));
+
+setImmediate(() => console.log('Immediate 1', timeStamp()));
 
 console.log('Programm end');
