22f3db6ec899dfa347905fb89e80191eedf002c6
Gregory Pereverzev <boot762@gmail.com>
2024-02-04 16:03:07 +0300

    faet: add final-fibonacci.js

--- 02-blocking-event-loop/final-fibonacci.js
+++ 02-blocking-event-loop/final-fibonacci.js
@@ -1,19 +1,57 @@
-function fib(position) {
-  if (position === 0 || position === 1) {
-    return position;
-  }
+// function fib(position) {
+//   if (position === 0 || position === 1) {
+//     return position;
+//   }
 
-  let prevPrev = 0;
-  let prev = 1;
-  let result;
+//   let prevPrev = 0;
+//   let prev = 1;
+//   let result;
 
-  for (let i = 1; i < position; i++) {
-    result = prevPrev + prev;
-    prevPrev = prev;
-    prev = result;
-  }
+//   for (let i = 1; i < position; i++) {
+//     result = prevPrev + prev;
+//     prevPrev = prev;
+//     prev = result;
+//   }
 
-  return result;
+//   return result;
+// }
+
+// console.log(fib(11));
+
+const fs = require('fs');
+
+console.log('Start');
+
+function promFib(position) {
+  return new Promise((resolve, reject) => {
+    if (position === 0 || position === 1) {
+      return position;
+    }
+
+    let prevPrev = 0;
+    let prev = 1;
+    let result;
+
+    for (let i = 1; i < position; i++) {
+      result = prevPrev + prev;
+      prevPrev = prev;
+      prev = result;
+    }
+
+    result > 100 ? resolve(result) : reject('Error 121');
+  });
 }
 
-console.log(fib(11));
+promFib(1000)
+  .then(res => res + '')
+  .then(res => res.split(''))
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+
+promFib(10)
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+
+console.log('End');
+
+console.log(module, '\n', exports, '\n', require);
