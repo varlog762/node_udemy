c74dc361690f14ed89dd8b3d4b249cec16554438
Gregory Pereverzev <boot762@gmail.com>
2024-02-28 19:25:27 +0300

    feat: install & run morgan

--- 12-express/04-middlewares/app.js
+++ 12-express/04-middlewares/app.js
@@ -1,13 +1,9 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(req.method, req.path);
-  next();
-};
-
-app.use(logger);
+app.use(morgan('combined'));
 
 app.use((req, res) => res.send('This is Express server!'));
 
