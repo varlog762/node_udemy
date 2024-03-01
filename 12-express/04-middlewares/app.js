9610102de2aa0298d25412f41babb97d3694ff36
Gregory Pereverzev <boot762@gmail.com>
2024-03-01 20:35:46 +0300

    feat: add comments to middleware lesson code

--- 12-express/04-middlewares/app.js
+++ 12-express/04-middlewares/app.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import morgan from 'morgan';
-import qs from 'querystring';
+// import qs from 'querystring';
 
 const app = express();
 
+// Use Morgan middleware to log incoming HTTP requests
 app.use(morgan('combined'));
 
 // Express middleware function for request body JSON parsing
 app.use(express.json());
 
-// Myown middleware function for request body JSON parsing
+/* Express middleware function for form data parsing
+express.urlencoded({ extended: true }) - uses 'qs' library for parsing form data, supports more features
+express.urlencoded({ extended: false }) - default behavior, uses buil-in module 'querystring'
+*/
+app.use(express.urlencoded({ extended: true }));
+
+// Example of custom middleware for JSON parsing:
 // app.use((req, res, next) => {
 //   let data = '';
 
@@ -22,25 +29,28 @@ app.use(express.json());
 //   });
 // });
 
-app.use((req, res, next) => {
-  if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
-    let data = '';
+// Example of custom middleware for form data parsing:
+// app.use((req, res, next) => {
+//   if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+//     let data = '';
 
-    req.on('data', chunk => (data += chunk.toString()));
-    req.on('end', () => {
-      const parsedFormData = qs.parse(data);
-      req.body = parsedFormData;
-      next();
-    });
-  } else {
-    next();
-  }
-});
+//     req.on('data', chunk => (data += chunk.toString()));
+//     req.on('end', () => {
+//       const parsedFormData = qs.parse(data);
+//       req.body = parsedFormData;
+//       next();
+//     });
+//   } else {
+//     next();
+//   }
+// });
 
+// Route handler to log and return the parsed request body
 app.use((req, res) => {
   console.log(req.body);
 
   return res.send(req.body);
 });
 
+// Route handler to log and return the parsed request body
 app.listen(5001, () => console.log('Server started on port 5001'));
