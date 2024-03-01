37ac4f6099fdb75170f6e363358e8cf7ee65911a
Gregory Pereverzev <boot762@gmail.com>
2024-03-01 18:40:05 +0300

    feat: parsing form data without middleware

--- 12-express/04-middlewares/app.js
+++ 12-express/04-middlewares/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import qs from 'querystring';
 
 const app = express();
 
@@ -21,10 +22,25 @@ app.use(express.json());
 //   });
 // });
 
+app.use((req, res, next) => {
+  if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+    let data = '';
+
+    req.on('data', chunk => (data += chunk.toString()));
+    req.on('end', () => {
+      const parsedFormData = qs.parse(data);
+      req.body = parsedFormData;
+      next();
+    });
+  } else {
+    next();
+  }
+});
+
 app.use((req, res) => {
   console.log(req.body);
 
-  return res.send('This is Express server!');
+  return res.send(req.body);
 });
 
 app.listen(5001, () => console.log('Server started on port 5001'));
